09340aaa674be78d1e7bb22f01fce66aa092ee2e
willrenner <willrenner2@gmail.com>
2021-01-29 16:10:47 -0600

    New stffff

--- scripts/stepperRunner.m
+++ scripts/stepperRunner.m
@@ -1,6 +1,8 @@
 clc;clear;
-hWaitbar = waitbar(0, 'Running...', 'Name', 'AARC','CreateCancelBtn','delete(gcbf)');
-% a = arduino('COM3', 'Mega2560');
+clear global;
+% hWaitbar = waitbar(0, 'Running...', 'Name', 'AARC','CreateCancelBtn','delete(gcbf)');
+global a;
+a = arduino('COM3', 'Mega2560');
 
 global leadScrewLead;
 global numSteps;
@@ -9,27 +11,47 @@ global prevTime;
 global timePerStep;
 global prevLogTime;
 global currentYpos;
+global stepperRevSpeedCmd;
+global directionCmd;
+global pulsePin;
+global directionPin;
+global dt;
+global loopCount;
 
-pulsePin = "D1";
-directionPin = "D2";
+loopCount = 0;
+
+
+dt = 0.001;
+
+pulsePin = "D2";
+directionPin = "D3";
 leadScrewLead = 8; %mm/rev
-numSteps = 200; %steps/rev, 1.8deg/step
+numSteps = 100; %steps/rev, 1.8deg/step
 currentYpos = 0; %once limit switch is reached
 directionCmd = "down";
 
+
 currentStep = 0;
-ROP = 1000; %mm/min
+ROP = 200; %mm/min
 run = true;
 prevTime = tic;
 prevLogTime = tic;
-
+pause on;
+time1 = tic;
 %****** wont work if  timeSincePrev >= 2*time per step, means will skip steps
 while(run)
-    if (~ishandle(hWaitbar)) % Stop if cancel button was pressed
-        disp('Stopped by user');
-        break;
+    loopCount = loopCount + 1;
+    if (toc(time1) >= 1)
+        disp("fps: " + loopCount);
+        loopCount = 0;
+        time1 = tic;
     end
+%     if (~ishandle(hWaitbar)) % Stop if cancel button was pressed
+%         disp('Stopped by user');
+%         break;
+%     end
     timeSincePrev = toc(prevTime);
+    writeDigitalPin(a, pulsePin, 0); %set pulse to low before anything
     checkIfTimeToStep(timeSincePrev);
     timePerStep = getTimeFromROP(ROP); %calculate timePerStep
     currentYpos = currentStep / numSteps * leadScrewLead; %mm from limit switch
@@ -43,8 +65,10 @@ end
 function timePerStep_ = getTimeFromROP(ropIn) %ROP in mm/min
     global leadScrewLead;
     global numSteps;
+    global stepperRevSpeedCmd;
     if (ropIn <= 0.5) %less than 0.5 mm per minute, just return max time per step so it won't move
         timePerStep_ = 9999; %sec/step
+        stepperRevSpeedCmd = 0;
     else
         stepperRevSpeedCmd = ropIn / leadScrewLead / 60; %rev/sec
         stepperRevSpeedCmd = stepperRevSpeedCmd * numSteps; %steps/sec
@@ -66,17 +90,23 @@ end
 function stepOnce(direction) 
     global currentStep;
     global prevTime;
-%     if (direction == "down")
-%         writeDigitalPin(a, directionPin, 1) %1 for down?
-%         writeDigitalPin(a, pulsePin, 1); %each step happens per pulse
-%         %sleep for a tiny bit??
+    global directionPin;
+    global pulsePin;
+    global dt;
+    global a;
+    if (direction == "down")
+        writeDigitalPin(a, directionPin, 1) %1 for down?
+        writeDigitalPin(a, pulsePin, 1); %each step happens per pulse
+        %sleep for a tiny bit??
+%         pause(dt);
 %         writeDigitalPin(a, pulsePin, 0);
-%     elseif (direction == "up")
-%         writeDigitalPin(a, directionPin, 0) %0 for up?
-%         writeDigitalPin(a, pulsePin, 1); %each step happens per pulse
-%         %sleep for a tiny bit??
+    elseif (direction == "up")
+        writeDigitalPin(a, directionPin, 0) %0 for up?
+        writeDigitalPin(a, pulsePin, 1); %each step happens per pulse
+        %sleep for a tiny bit??
+%         pause(dt);
 %         writeDigitalPin(a, pulsePin, 0);
-%     end
+    end
         prevTime = tic;
         currentStep = currentStep + 1; %increment stepper motor step        
 end
@@ -85,7 +115,9 @@ function logData()
     global currentStep;
     global currentYpos;
     global timePerStep;
+    global stepperRevSpeedCmd;
     disp("Step count: " +  currentStep);
     disp("Y pos (mm): " +  currentYpos);
     disp("Time per step: " +  timePerStep);
+    disp("Steps per sec: " +  stepperRevSpeedCmd);
 end
--- scripts/tests/stepperTest.m
+++ scripts/tests/stepperTest.m
@@ -5,21 +5,31 @@ hWaitbar = waitbar(0, 'Running...', 'Name', 'AARC','CreateCancelBtn','delete(gcb
 pulsePin = "D2";
 directionPin = "D3";
 dt = 0.1;
+loopCount = 0;
 
 writeDigitalPin(a,directionPin,1);
 % configurePin(a, pulsePin,
 pause on;
+time1 = tic;
 while (1)
-    if (~ishandle(hWaitbar)) % Stop if cancel button was pressed
-        disp('Stopped by user');
-        break;
+    writeDigitalPin(a,pulsePin,0);
+%     disp('LOW');
+    loopCount = loopCount + 1;
+    if (toc(time1) >= 1)
+%         disp("fps: " + loopCount);
+        loopCount = 0;
+        time1 = tic;
     end
+%     if (~ishandle(hWaitbar)) % Stop if cancel button was pressed
+%         disp('Stopped by user');
+%         break;
+%     end
 %   5 steps per second?
     writeDigitalPin(a,pulsePin,1);
-    disp('HIGH');
-    pause(dt);
-    writeDigitalPin(a,pulsePin,0);
-    disp('LOW');
-    pause(dt);
+%     disp('HIGH');
+%     pause(dt);
+    
+    
+%     pause(dt);
 end
 
