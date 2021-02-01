fb462cc2d4b95a217e751a62920caf1e0f627594
Will Renner <willrenner2@gmail.com>
2021-02-01 13:31:37 -0600

    New

---
+++ arduino/stepperRunner.ino
@@ -0,0 +1,19 @@
+#include <AccelStepper.h>
+
+const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+AccelStepper stepper;
+
+int stepCount = 0; // number of steps the motor has taken
+
+void setup()
+{
+}
+
+void loop()
+{
+
+    // int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+}
