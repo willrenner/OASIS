7dca80ee8f85a9be3e1c5dc294f6fed4be13f55a
willrenner <willrenner2@gmail.com>
2021-04-07 13:34:53 -0500

    Update main.ino

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -399,8 +399,9 @@ void fpsCounter() {
 void getdrillVoltageCurrent() {
 
 }
-void getCurrentSensorValue() {
+bool getCurrentSensorValue() {
     float val = -0.04757 * analogRead(currentSensorPin) + 24.36; //eqn to get amperage, y = mx + b by testing
     val = sqrt(pow(val, 2) + pow(drillCurrent, 2)); //kinda RMS not really, may not even work who knows
     drillCurrent = 0.1*val + 0.9*drillCurrent; //complementary filter
+    return true;
 }
